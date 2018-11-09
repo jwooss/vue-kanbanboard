a1756311798940edc64a3678001019d8a8ba0633
KimJaeWoos <wodn4131@gmail.com>
2018-11-09 21:55:16 +0900

    기본 라우팅

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+const Login = { template: '<div>Login page</div>'}
+
+const routes = {
+  '/': App,
+  '/Login': Login
+}
+
 new Vue({
   el: '#app',
-  render: h => h(App)
+  computed: {
+    VueComponent() {
+      return routes[window.location.pathname] ||
+        { template: '<div>404 not found</div>'}
+    }
+  },
+  render(h) {
+    return h(this.VueComponent)
+  }
 })
