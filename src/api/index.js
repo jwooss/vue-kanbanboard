c526a042a23f827a5524e94a00b03daedd9d1f8b
KimJaeWoos <wodn4131@gmail.com>
2018-11-10 16:58:53 +0900

    refactor: add axios helper

---
+++ src/api/index.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+import router from '../router'
+
+const Domain = 'http://localhost:3000'
+const UNAUTHORIZED = 401
+const onUnauthorized = () => {
+  router.push('/login')
+}
+
+const request = (method, url, data) => {
+  return axios({
+    method,
+    url : Domain + url,
+    data: data
+  }).then(result => result.data)
+    .catch(result => {
+      const { status } = result.response
+      if (status === UNAUTHORIZED) return onUnauthorized()
+      throw Error(result)
+    })
+}
+
+export const board = {
+  fetch() {
+    return request('get', '/boards')
+  }
+}
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -28,7 +28,7 @@
 </template>
 
 <script>
-  import axios from 'axios';
+  import {board} from './../api';
 
   export default {
     data() {
@@ -44,16 +44,9 @@
 
       fetchData() {
         this.loading = true;
-
-        axios.get('http://localhost:3000/boards')
-          .then(res => {
-            this.boards = res.data
-          })
-          .catch(res => {
-            this.$router.replace('/login')
-          })
-          .finally(() => {
-            this.loading = false
+        board.fetch()
+          .then(data => {
+            this.boards = data
           })
       }
     },
