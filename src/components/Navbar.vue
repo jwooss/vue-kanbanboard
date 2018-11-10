9c60efd7ee1ee8dea7e8ac0880db76f9a0a87754
KimJaeWoos <wodn4131@gmail.com>
2018-11-10 15:00:13 +0900

    add router-link

---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <router-link to="/">home</router-link>
+      <router-link to="/login">login</router-link>
+    </div>
+</template>
+
+<script>
+
+
+    export default {
+        name: "Navbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
