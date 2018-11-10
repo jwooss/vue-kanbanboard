76112a7f1231ee348e561c2587672942b4658c40
KimJaeWoos <wodn4131@gmail.com>
2018-11-10 16:32:00 +0900

    add api-basic

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,30 +1,63 @@
 <template>
+  <div>
+    Home
     <div>
-      Home
-      <div>
-        Board List:
-        <ul>
-          <li>
-            <router-link to="/b/1">Board 1</router-link>
-          </li>
-          <li>
-            <router-link to="/b/2">Board 2</router-link>
-          </li>
-          <li>
-            <router-link to="/b/3">Board 3</router-link>
-          </li>
-          <li>
-            <router-link to="/b/4">Board 4</router-link>
-          </li>
-        </ul>
+      Board List:
+      <div v-if="loading">loading...</div>
+      <div v-else>
+        Api reulst : {{apiRes}}
       </div>
+      <ul>
+        <li>
+          <router-link to="/b/1">Board 1</router-link>
+        </li>
+        <li>
+          <router-link to="/b/2">Board 2</router-link>
+        </li>
+        <li>
+          <router-link to="/b/3">Board 3</router-link>
+        </li>
+        <li>
+          <router-link to="/b/4">Board 4</router-link>
+        </li>
+      </ul>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Home"
-    }
+  export default {
+    data() {
+      return {
+        loading : false,
+        apiRes: ''
+      }
+    },
+    created() {
+      this.fetchData();
+    },
+    methods: {
+
+      fetchData() {
+        this.loading = true;
+        const req = new XMLHttpRequest();
+
+        req.open('GET', 'http://localhost:3000/health');
+
+        req.send()
+
+        req.addEventListener('load', () => {
+          this.loading = false;
+          this.apiRes = {
+            status: req.status,
+            statusText: req.statusText,
+            response: JSON.parse(req.response)
+          }
+        })
+      }
+    },
+    name: "Home"
+  }
 </script>
 
 <style scoped>
